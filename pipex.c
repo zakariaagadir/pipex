416cb18eeb6f348172d69998cfb2be55136c8be1
Zakaria Mounji <zmounji@e2r5p1.1337.ma>
2025-01-09 18:09:14 +0100

    testeing to fixing leaks in file discriptor

---
+++ pipex.c
@@ -0,0 +1,69 @@
+#include "pipex.h"
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+void	exec(char *cmd, char **env)
+{
+	char	**s_cmd;
+	char	*path;
+
+	s_cmd = ft_split(cmd, ' ');
+	path = get_path(s_cmd[0], env);
+	if (execve(path, s_cmd, env) == -1)
+	{
+		perror ("pipex: command not found: ");
+		perror (s_cmd[0]);
+		ft_free_tab(s_cmd);
+		exit(0);
+	}
+}
+
+void	child(char **av, int *p_fd, char **env)
+{
+	int		fd;
+
+	fd = open_file(av[1], 0);
+	dup2(fd, 0);
+	dup2(p_fd[1], 1);
+	close(p_fd[0]);
+	close(fd); // close
+	exec(av[2], env);
+}
+
+void	parent_process(char **av, int *p_fd, char **env)
+{
+	int		fd;
+
+	fd = open_file(av[4], 1);
+	dup2(fd, 1);
+	dup2(p_fd[0], 0);
+	close(p_fd[1]);
+	close(fd); // close
+	exec(av[3], env);
+}
+
+int	main(int ac, char **av, char **env)
+{
+	int		p_fd[2];
+	pid_t	pid;
+
+	if (ac != 5)
+		exit_handler(1);
+	if (pipe(p_fd) == -1)
+		exit(-1);
+	pid = fork();
+	if (pid == -1)
+		exit(-1);
+	if (!pid)
+		child(av, p_fd, env);
+	parent_process(av, p_fd, env);
+}
---
+++ pipex.h
@@ -0,0 +1,22 @@
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include<sys/types.h>
+# include<sys/stat.h>
+# include <fcntl.h>  
+# include <stdlib.h>
+
+int		open_file(char *file, int n);
+int     ft_strcmp(const char *s1, const char *s2);
+char	*my_getenv(char *name, char **env);
+char	*get_path(char *cmd, char **env);
+void	exec(char *cmd, char **env);
+void	ft_free_tab(char **tab);
+void	exit_handler(int n_exit);
+
+
+#endif
